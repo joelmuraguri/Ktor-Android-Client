0b74fb5b9184c92efc42c266ab8d989a69c172fc
Joel-K-Muraguri <joelkmuraguri21@gmail.com>
2023-03-01 16:12:35 +0300

    create project

---
+++ app/src/androidTest/java/com/joel/ktor_android_client_calls/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.joel.ktor_android_client_calls
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.joel.ktor_android_client_calls", appContext.packageName)
+    }
+}
