49d511e7f79937fa10dfe732bd85ac4f9ff5f165
Joel-K-Muraguri <joelkmuraguri21@gmail.com>
2023-03-05 10:09:23 +0300

    set up network calls

--- app/src/main/java/com/joel/ktor_android_client_calls/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/joel/ktor_android_client_calls/presentation/activity/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.joel.ktor_android_client_calls.presentation.movie.MovieList
 import com.joel.ktor_android_client_calls.presentation.ui.theme.KtorAndroidClientCallsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
 
-
+                    MovieList()
                 }
             }
         }
